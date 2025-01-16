325218b6f02806eab7caa34466e7d352582e595f
Davidwasfy11 <164547161+Davidwasfy11@users.noreply.github.com>
2025-01-16 17:45:25 +0200

    just a fake push to origin development

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -7,6 +7,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
+        
         title: const Text('Login screen'),
       ),
       body: const Center(
